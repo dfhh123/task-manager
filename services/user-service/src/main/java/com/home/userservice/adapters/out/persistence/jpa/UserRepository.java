1f9246ed986503e4bfa43187cff5e8548249f906
dfhh12 <biryulin2003@gmail.com>
2025-10-07 22:46:24 +0300

    add repository

---
+++ services/user-service/src/main/java/com/home/userservice/adapters/out/persistence/jpa/UserRepository.java
@@ -0,0 +1,8 @@
+package com.home.userservice.adapters.out.persistence.jpa;
+
+import com.home.userservice.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+}
