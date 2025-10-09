412f57aae6df4e45d3f713c5a6e6c090d029dbc7
dfhh12 <biryulin2003@gmail.com>
2025-10-09 15:01:00 +0300

    change id from long to UUID

--- services/user-service/src/main/java/com/home/userservice/domain/entity/User.java
+++ services/user-service/src/main/java/com/home/userservice/domain/entity/User.java
@@ -3,6 +3,8 @@ package com.home.userservice.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.UUID;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -11,7 +13,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Long id;
+    private UUID id;
 
     @Column(name = "email", nullable = false, unique = true)
     private String email;
